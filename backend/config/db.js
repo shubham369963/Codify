c692d844443d53d76bab83b30f7785f48e586211
shubham369963 <shubhamhalade369@gmail.com>
2023-08-08 17:58:11 +0530

    connected backend to the mongodb database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
+const connectDB = async() => {
     try{
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
         });
-        console.log(`mongodb connected: ${conn.connection.host}`);
+        console.log(`mongodb connected to host: ${conn.connection.host}`);
     }catch(err){
         console.error(`Error: ${err}`);
         process.exit();
