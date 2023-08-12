23a375f3ee81455f16766ece9fa50791c4e7ebbb
shubham369963 <shubhamhalade369@gmail.com>
2023-08-12 19:46:08 +0530

    removed url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
+mongoose.set('bufferCommands', false);
 const connectDB = async()=>{
     try{
         console.log(process.env.MONGO_URI);
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log(`mongodb connected to host: ${conn.connection.host}`)
     }catch(err){
         console.error(`Error: ${err}`);
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
-dotenv.config();
 const port = process.env.PORT || 5000;
 const codes = require("./data/codes.js");
 const connectDB = require("./config/db.js");
@@ -9,7 +7,8 @@ const userRoutes = require("./routes/userRoutes.js");
 const codeRoutes = require("./routes/codeRoutes.js");
 const {notFound, errorHandler} = require("./middlewares/errorMiddleware.js");
 const path = require("path");
-
+const dotenv = require("dotenv");
+dotenv.config();
 connectDB();
 app.use(express.json());
 
