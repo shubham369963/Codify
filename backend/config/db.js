af94489ac8c21f28ef0262f22f657f320a9181c7
shubham369963 <shubhamhalade369@gmail.com>
2023-08-12 19:35:19 +0530

    removed url parser from db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-
-const connectDB = async() => {
+mongoose.set('strictQuery', false);
+const connectDB = async()=>{
     try{
         console.log(process.env.MONGO_URI);
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
-        });
-        console.log(`mongodb connected to host: ${conn.connection.host}`);
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`mongodb connected to host: ${conn.connection.host}`)
     }catch(err){
         console.error(`Error: ${err}`);
         process.exit();
